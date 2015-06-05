364f7cc506ac3a939210711164d543f3b845e690
Sourabh Desai <sourabhdesai@Sourabhs-MacBook-Air.local>
2015-06-04 22:04:00 -0700

    added recording controller tests

---
+++ test/unit/controllers/RecordingController.test.js
@@ -0,0 +1,46 @@
+/**
+ * Tests done with mocha, chai, and supertest
+ * Mocha: http://mochajs.org/
+ * Chai: http://chaijs.com/
+ * Supertest: https://github.com/visionmedia/supertest
+ * 	Supertest is based on superagent.
+ * "Anything you can do with superagent, you can do with supertest" - Supertest README.md
+ * 	Superagent: https://visionmedia.github.io/superagent/
+ * More info on Testing in Sails: http://sailsjs.org/#!/documentation/concepts/Testing
+ */
+
+var request = require('supertest');
+var chai    = require("chai");
+var fs      = require('fs');
+
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should();
+
+var videoFile = "./test/test_assets/jmarr.mp4"; // Test file that tests will be uploading
+
+describe("Test Uploading and Downloading of Video", function () {
+
+	describe("Test Uploading of Video", function () {
+		it("Should successfully upload the video to the server", function (done) {
+			request(sails.hooks.http.app)
+				.post("/postVideo/jmarr-cpy.mp4")
+				.attach("video", videoFile)
+				.expect(200, done);
+		});
+	});
+
+	describe("Test Uploading of Video", function () {
+		it("Should successfully download the video from the server", function (done) {
+			request(sails.hooks.http.app)
+				.get("/getVideo/jmarr-cpy.mp4")
+				.expect(function (res) {
+					var stats    = fs.statSync(videoFile);
+					var fileSize = stats.size;
+					assert.equal(res.header['content-length'], fileSize, "Uploaded file and downloaded file have different lengths");
+				})
+				.expect(200, done);
+		});
+	});
+
+});
