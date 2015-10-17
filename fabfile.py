7de33130b10946583d3a4063f4a49ea628f9dd29
sourabhdesai <sourabhdesai@gmail.com>
2015-10-16 23:27:47 -0500

    added checking for videos dir in deployment script

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,7 @@ from fabric.api import env, local, run, cd, sudo, open_shell, settings
 #   Installing Node: http://askubuntu.com/questions/672994/how-to-install-nodejs-4-on-ubuntu-15-04-64-bit-edition
 #   Installing Git: https://www.digitalocean.com/community/tutorials/how-to-install-git-on-ubuntu-14-04
 #   Fixing npm global install issues: https://docs.npmjs.com/getting-started/fixing-npm-permissions
+#   Redirecting to port 80: http://richardfergie.com/redirect-port-80-to-a-different-port-using-iptables
 
 env.user = ""
 env.password = ""
@@ -12,6 +13,7 @@ env.hosts = ["classcapture1.cs.illinois.edu"]
 
 git_base_url = "https://github.com/sourabhdesai/"
 git_repo_name = "ClassCapture_HTTPServer"
+videos_dir = "~/classcapture_videos"
 
 def test():
     """
@@ -25,7 +27,7 @@ def checkout():
     Also, changes current directory to the checked out repo
     """
     must_clone_repo = False
-    with settings(warn_only=True): # makes it so that test -d won't cause an interruption if it fails
+    with settings(warn_only=True): # makes it so that `test -d <dirname>` won't cause an interruption if it fails
         must_clone_repo = run("test -d %s" % git_repo_name).failed
     if must_clone_repo:
         run("git clone %s%s.git" % (git_base_url, git_repo_name))
@@ -35,10 +37,16 @@ def checkout():
 
 def start_server():
     """
+    First check if video directory exists, make it if it doesn't
     Start the server on the VM
     uses pm2 to start & manager cluster of server processes
     """
-    run("pm2 start app.js -i 0")
+    must_create_videos_dir = False
+    with settings(warn_only=True): # makes it so that `test -d <dirname>` won't cause an interruption if it fails
+        must_create_videos_dir = run("test -d %s" % videos_dir).failed
+    if must_create_videos_dir:
+        run("mkdir %s" % videos_dir)
+    run("pm2 start app.js -i 0 -x -- --prod")
 
 def deploy(user, password, shell_before=False, shell_after=True):
     env.user = user
