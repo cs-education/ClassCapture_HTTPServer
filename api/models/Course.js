718dae793a3eeb4b409c927f3e86872638aafb41
Alvin Jou <alvjou@gmail.com>
2015-11-29 14:43:57 -0600

    updated course model to validate via course API, and added test cases

--- api/models/Course.js
+++ api/models/Course.js
@@ -2,7 +2,7 @@
 * Course.js
 *
 * @description :: Schema for Courses with relevant course metadata and linking to specific course sections
-* @docs        :: http://sailsjs.org/#!documentation/models
+* @docs        : http://sailsjs.org/#!documentation/models
 */
 
 const DEPT_MIN_LENGTH = 2;
@@ -28,7 +28,34 @@ module.exports = {
 		"sections": { // Example: CS 225 may have 2 lecture sections, 5 lab sections, & 5 discussion sections
 			"collection": "Section",
 			"via": "course"
+		},
+			"year": { // Example: CS 225 in the year 2015
+			"type": "integer",
+			"required": true
+		},
+		"semester": { // Example: fall, spring, or summer semesters.
+			"type": "string",
+			"enum": ["spring", "summer", "fall"],
+			"required" : true
 		}
-	}
+	},
+
+	// Lifecycle Callbacks
+	beforeCreate: function(values, cb){
+		// Validate that the course is a valid course
+		var url = "http://courses.illinois.edu/cisapp/explorer/schedule/" + values.year + "/" + values.semester + "/" + values.department +"/" + values.number + ".xml";
+		var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+		var http = new XMLHttpRequest();
+		http.open('HEAD', url, false);
+		http.send();
+		var courseExists = (http.status!=404);
+		if (courseExists) {
+			cb();
+		}
+		else {
+			cb(new Error());
+		}
+
+	} 
 };
 
--- test/seed_scripts/Course.seed.test.js
+++ test/seed_scripts/Course.seed.test.js
@@ -10,35 +10,98 @@ var should = chai.should();
 
 const NUM_COURSES = 1;
 
+function testSuccessCreateCourse(dept, num, semester, year) {
+	const MOCK_DEVICE_ID = "TESTTEST$$TESTTEST";
+	it(`Should create a Course Entry called "${dept} ${num}, ${semester} ${year}"`, done => {
+		request(sails.hooks.http.app)
+			.post('/course/')
+			.set(BlacklistService.DEVICE_ID_HEADER_NAME, MOCK_DEVICE_ID)
+			.send({
+				"department": dept,
+				"number": num,
+				"year": year,
+				"semester": semester
+			})
+			.expect(res => {
+				var courseBody = res.body;
+				courseBody.should.have.property('department');
+				courseBody.department.should.equal(dept);
+				courseBody.should.have.property('number');
+				courseBody.number.should.equal(num);
+				courseBody.should.have.property('id');
+				courseBody.should.have.property('year');
+				courseBody.year.should.equal(year);
+				courseBody.should.have.property('semester');
+				courseBody.semester.should.equal(semester);
+
+				// Upon creation, the returned object doesn't have the sections property
+				// Manually add it here so it can be referenced in future tests
+			 	courseBody.sections = [];
+			})
+			.expect(201, done);
+	});
+}
+// Invalid department causes an 'E_VALIDATION' error, which is a 400 error
+function testInvalidDeptCreateCourse(dept, num, semester, year) {
+	const MOCK_DEVICE_ID = "TESTTEST$$TESTTEST";
+	it(`Should NOT create a Course Entry called "${dept} ${num}, ${semester} ${year}"`, done => {
+		request(sails.hooks.http.app)
+			.post('/course/')
+			.set(BlacklistService.DEVICE_ID_HEADER_NAME, MOCK_DEVICE_ID)
+			.send({
+				"department": dept,
+				"number": num,
+				"year": year,
+				"semester": semester
+			})
+			.expect(res => {
+				res.body.error.should.equal('E_VALIDATION');
+				res.body.status.should.equal(400);;
+			})
+			.expect(400, done)
+	});
+}
+// Invalid section causes an 'E_UNKNOWN' error, which is a 500 error
+function testInvalidSectionCreateCourse(dept, num, semester, year) {
+	const MOCK_DEVICE_ID = "TESTTEST$$TESTTEST";
+	it(`Should NOT create a Course Entry called "${dept} ${num}, ${semester} ${year}"`, done => {
+		request(sails.hooks.http.app)
+			.post('/course/')
+			.set(BlacklistService.DEVICE_ID_HEADER_NAME, MOCK_DEVICE_ID)
+			.send({
+				"department": dept,
+				"number": num,
+				"year": year,
+				"semester": semester
+			})
+			.expect(res => {
+				res.body.error.should.equal('E_UNKNOWN');
+				res.body.status.should.equal(500);;
+			})
+			.expect(500, done)
+	});
+}
+
 describe(`Create ${NUM_COURSES} Course Entries`, () => {
 	// Make sure that you've added a DeviceID to each request to pass the Blacklisting policy
-	const MOCK_DEVICE_ID = "TESTTEST$$TESTTEST";
 
-	_.times(NUM_COURSES, () => {
-		const courseDept = chance.string({length: 3, pool: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'});
-		const courseNum = chance.natural({min: 100, max: 600});
+	var courseDept = "CS";
+	var courseNum = 225;
+	var semester = "fall";
+	var year = 2015;
+	testSuccessCreateCourse(courseDept, courseNum, semester, year);
 
-		it(`Should create a Course Entry called "${courseDept} ${courseNum}"`, done => {
-			request(sails.hooks.http.app)
-				.post('/course/')
-				.set(BlacklistService.DEVICE_ID_HEADER_NAME, MOCK_DEVICE_ID)
-				.send({
-					"department": courseDept,
-					"number": courseNum
-				})
-				.expect(res => {
-					var courseBody = res.body;
-					courseBody.should.have.property('department');
-					courseBody.department.should.equal(courseDept);
-					courseBody.should.have.property('number');
-					courseBody.number.should.equal(courseNum);
-					courseBody.should.have.property('id');
+	courseDept = "CS";
+	courseNum = 410;
+	semester = "spring"
+	testSuccessCreateCourse(courseDept, courseNum, semester, year);
 
-					// Upon creation, the returned object doesn't have the sections property
-					// Manually add it here so it can be referenced in future tests
-					courseBody.sections = [];
-				})
-				.expect(201, done);
-		});
-	});
+	// CS410 is only offered in the spring, so fall should fail
+	semester = "fall";
+	// Actually causes the 500 (server error) response to be received in the terminal. Not sure how to surpress that, but test still passes. 
+	testInvalidSectionCreateCourse(courseDept, courseNum, semester, year);
+
+	// Test invalid department
+	courseDept = "xyz";
+	testInvalidDeptCreateCourse(courseDept, courseNum, semester, year);
 });
