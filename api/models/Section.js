62d5cc2e0344876b934af2192f28956dab9b5130
sourabhdesai <sourabhdesai@gmail.com>
2016-01-25 16:02:07 -0600

    added optional echo360 id field to sections

--- api/models/Section.js
+++ api/models/Section.js
@@ -6,9 +6,15 @@
 */
 
 const MIN_SECTION_NAME_LENGTH = 2;
+const UUID_PATTERN = new RegExp(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
 
 module.exports = {
-
+	"types": {
+		"isUUID": uuid => {
+			var matches = uuid.match(UUID_PATTERN);
+			return _.isArray(matches) && matches.length === 1;
+		}
+	},
 	"attributes": {
 		// Common example of section name: "AL1"
 		"name": {
@@ -24,6 +30,10 @@ module.exports = {
 			"collection": "User",
 			"via": "sections",
 			dominant: true
+		},
+		"echo360UUID": {
+			"type": "string",
+			"isUUID": true
 		}
 	}
 };
